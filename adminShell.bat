58c7c8a532a6d1cf5e4f40b291b3465f643a0c41
Jani Ahlgren <jani.ahlgren@varjo.com>
2021-10-28 16:55:15 +0300

    add automatic admin priviledge

--- adminShell.bat
+++ adminShell.bat
@@ -1,4 +1,33 @@
+:::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights
+:::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS 
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
 
+:checkPrivileges 
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges ) 
+
+:getPrivileges 
+if '%1'=='ELEV' (shift & goto gotPrivileges)  
+ECHO. 
+ECHO **************************************
+ECHO Getting Adming rights 
+ECHO **************************************
+
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs" 
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs" 
+"%temp%\OEgetPrivileges.vbs" 
+exit /B 
+
+:gotPrivileges 
 ::::::::::::::::::::::::::::
 ECHO  START install process
 ::::::::::::::::::::::::::::
